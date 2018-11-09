44b3386ca02e23177435c36ce50591e760f5445f
huangzecheng928 <283966973@qq.com>
2018-11-09 22:25:10 +0800

    commit pull local changes

---
+++ src/test/java/com/example/controller/login.java
@@ -0,0 +1,11 @@
+package com.example.controller;
+
+public class login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("the system is testing!!");
+	}
+
+}
